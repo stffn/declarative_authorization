7b82ab8eb926603802cc6f66a5c6573eda78c867
Steffen Bartsch <sbartsch@tzi.org>
2009-02-25 19:43:57 +0100

    Check for minimum Rails version

--- init.rb
+++ init.rb
@@ -4,6 +4,11 @@ require File.dirname(__FILE__) + "/lib/in_controller.rb"
 require File.dirname(__FILE__) + "/lib/in_model.rb"
 require File.dirname(__FILE__) + "/lib/obligation_scope.rb"
 
+min_rails_version = "2.1.0"
+if Rails::VERSION::STRING < min_rails_version
+  raise "declarative_authorization requires Rails #{min_rails_version}.  You are using #{Rails::VERSION::STRING}."
+end
+
 ActionController::Base.send :include, Authorization::AuthorizationInController
 ActionController::Base.helper Authorization::AuthorizationHelper
 
