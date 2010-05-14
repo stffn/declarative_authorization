ad95b833bfbac7952c625add9181448bf476b447
TJ Singleton <tjsingleton@vantagestreet.com>
2010-05-13 23:46:12 -0400

    Use Rails 3's prefered interface while maintaining backwards compatibility.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
-ActionController::Routing::Routes.draw do |map|
+# Rails 3 depreciates ActionController::Routing::Routes 
+routes = (Rails.respond_to?(:application) ? Rails.application.routes : ActionController::Routing::Routes)
+
+routes.draw do |map|
   if Authorization::activate_authorization_rules_browser?
-    map.resources :authorization_rules, :only => [:index], 
+    map.resources :authorization_rules, :only => [:index],
         :collection => {:graph => :get, :change => :get, :suggest_change => :get}
     map.resources :authorization_usages, :only => :index
   end
