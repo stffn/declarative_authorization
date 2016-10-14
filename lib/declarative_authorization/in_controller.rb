ca173b69ec924dbbc27ec992607401db5cac9a9c
Fernando Chucre <fernandochucre@gmail.com>
2016-10-14 16:53:40 -0300

    fix rails 5 bug

--- lib/declarative_authorization/in_controller.rb
+++ lib/declarative_authorization/in_controller.rb
@@ -6,8 +6,8 @@ module Authorization
   
     def self.included(base) # :nodoc:
       base.extend(ClassMethods)
-      base.hide_action :authorization_engine, :permitted_to?,
-        :permitted_to!
+      #base.private_class_method :authorization_engine, :permitted_to?,
+      #  :permitted_to!
     end
     
     DEFAULT_DENY = false
